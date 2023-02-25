4a0b1e9a6edcfaaaba02553309ee1f01b56ad4ef
Vittal cg <50832845+Vittalgc@users.noreply.github.com>
2023-02-25 21:54:48 +0530

    final commit

--- app.py
+++ app.py
@@ -1,77 +1,38 @@
 from flask import Flask, request, render_template, redirect, url_for
-#import pandas as pd
-#from scipy.misc import imread, imresize
-import base64
-from keras.models import load_model
 import numpy as np
-import cv2
-from werkzeug.utils import secure_filename
-import os
-#init_Base64 = 21
-
-global model
-inp = None
+from PIL import Image
+from keras.models import load_model
 
 app = Flask(__name__, template_folder='templates')
+global model,prediction
 
-#@app.route('/')
-#def index_view():
-#    return render_template('home.html')
-
-#def convertImage(imgData1):
-#	imgstr = re.search(b'base64,(.*)',imgData1).group(1)
-#	with open('output.png','wb') as output:
-#	    output.write(base64.b64decode(imgstr))
-
-#import our trained CNN from the h5 file
+# Loading the trained model from h5 file
 model = load_model("mnist.h5")
 
-app.config['IMAGE_UPLOADS'] = './static/Images'
+# route for the landing page or home page
+@app.route('/')
+def upload():
+    return render_template('home.html')
 
-@app.route('/home',methods=['POST',"GET"])
+# route for handling the uploaded image and predicting
+@app.route('/results', methods = ['POST'])
 def upload_image():
-    if request == "POST":
-        inp = request.files['file']
-        if inp.filename == "":
-            print("Invalid File name")
-            return redirect(request.url)
-        filename = secure_filename(inp.filename)
-
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        inp.save(os.path.join(basedir, app.config["IMAGE_UPLOADS"],filename))
-        return render_template("home.html",filename=filename)
-    
-    return render_template("home.html")
-
-@app.route('/display/<filename>')
-def display_image(filename):
-    return redirect(url_for('static',filename = "/Images" + filename), code = 888)
-
-@app.route('/predict', methods=['POST'])
-def predict():
     if request.method == 'POST':
-        prediction = None
+        # fetching the uploaded image and preprocessing it
+        img = Image.open(request.files['Img'].stream).convert("L")
+        img = img.resize((28,28))
+        img_arr = np.array(img).reshape(1,28,28,1)
 
-        #img = request.form['url']
-        #Removing the extra part of the url that was added for the encoding process
-        #img = img[init_Base64:]
+        # predict the digit
+        prediction = str(np.argmax(model.predict(img_arr)))
 
-        #Decoding for processing
-        #img_decoded = base64.b64decode(img)
-        #image = np.asarray(bytearray(img_decoded), dtype="uint8")
-        image = inp
-        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+        # rendering the results page with the predicted digit
+        return render_template('results.html',predicted = prediction)
 
-        #Resizing and reshaping the user's image.
-        resized = cv2.resize(image, (28,28), interpolation = cv2.INTER_AREA)
-        vect = np.asarray(resized, dtype="uint8")
-        vect = vect.reshape(1, 1, 28, 28).astype('float32')
-        #Predicting the digit
-        y_pred = model.predict(vect)
-        #To filter out the number the cnn has the most confidence in
-        prediction = np.argmax(y_pred)
-
-    return render_template('results.html', Prediction=prediction)
+# route for the "go home" button on the results page
+@app.route('/home')
+def home():
+    return render_template('home.html')
     
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
